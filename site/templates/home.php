d9a8f4f581074d74d6fbfaf082b08c62a21e8fc2
Arthur Schmitt <tart2000design@gmail.com>
2017-03-20 16:12:09 -0400

    Learning xp getting there

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,50 +1,42 @@
 <?php snippet('header') ?>
 
-<?php $projects = page('projects')->children()->sortBy('title','asc') ?>
-
-<header class="page pad front">
-  <h1>Fiches projet - benchmark YMCA</h1>
-  <div class="">
-    <?= $page->intro()->kirbytext() ?>
-  </div>
+<header class="page pad front" 
+  <?php if ($page->hasImages()) : ?>
+    style="background-image:url('<?php echo $page->images()->first()->url() ?>')"
+  <?php endif ?>
+  >
+  <h1><?php echo $page->intro() ?></h1>
   <hr>
   <?= $page->text()->kirbytext() ?>
   <img class="main-logo" src="<?php echo $site->url() ?>/assets/images/logo.png">
 </header>
 
-<div class="page pad">
-  <h1>Projets présentés</h1>
-    <?php echo page('projects')->text()->kirbytext() ?>
+<?php foreach ($site->pages()->visible() as $pa) : ?>
+  <?php if ($pa->template()=='projects') : ?>
+    <div class="page pad">
+      <h1><?php echo $pa->title() ?></h1>
+        <?php echo $pa->text()->kirbytext() ?>
+    </div>
     <?php $counter = 1 ?>
-    <hr>
-    <p>
-      <?php foreach ($projects as $p) :  ?>
-        <strong><?php echo $p->title() ?></strong> - p.<?php echo $counter ?> / 
-        <?php $counter++ ?>
-      <?php endforeach ?>
-    </p>
-    <hr>
-    <h3>Catégories</h3>
-    <?php foreach (page('categories')->children() as $cat) : ?>
-      <span class="facats">
-        <span class="fa-stack fa-lg">
-          <i class="fa fa-circle fa-stack-2x"></i>
-          <i class="fa fa-<?php echo $cat->fa() ?> fa-stack-1x fa-inverse"></i>
-        </span>
-        <span class="cat-title"><?php echo $cat->title() ?></span>
-      </span>
+    <?php foreach ($pa->children() as $p) : ?> <!-- pages projet -->
+      <div class="page">
+        <?php snippet('project', array('p'=>$p,'counter'=>$counter)) ?>
+      </div>
+      <?php $counter++ ?>
     <?php endforeach ?>
 
-</div>
+  <?php else : ?>
+    <div class="page pad">
+      <?php echo $pa->text()->kirbytext() ?>
+    </div>
+  <?php endif ?>
 
-<?php $counter = 1 ?>
 
-<?php foreach ($projects as $p) : ?> <!-- pages projet -->
-  <div class="page">
-    <?php snippet('project', array('p'=>$p,'counter'=>$counter)) ?>
-  </div>
-  <?php $counter++ ?>
 <?php endforeach ?>
+
+
+
+
 
 
 
